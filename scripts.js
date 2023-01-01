282312a7e4772db8d1023920e65e82ebb9fd6b91
Grace Lee <gracelee9901@gmail.com>
2023-01-01 13:15:33 -0800

    Add functionality to update scores and hold button

--- scripts.js
+++ scripts.js
@@ -8,8 +8,17 @@
 const rollPlayer1 = document.querySelector('#player1Roll');
 const rollPlayer2 = document.querySelector('#player2Roll');
 
+const holdBtnPlayer1 = document.querySelector('.hold-btn-p1');
+const holdBtnPlayer2 = document.querySelector('.hold-btn-p2');
+
 const rollBtnPlayer1 = document.querySelector('#roll-p1');
 const rollBtnPlayer2 = document.querySelector('#roll-p2');
+
+const player1ScoreTotal = document.getElementById('player1Score');
+const player2ScoreTotal = document.getElementById('player2Score');
+
+const player1RoundScore = document.getElementById('player1Round');
+const player2RoundScore = document.getElementById('player2Round');
 
 
 // Scores
@@ -18,36 +27,74 @@ let playerTwoTotalScore = 0;
 let playerOneRoundScore = 0;
 let playerTwoRoundScore = 0;
 
-// Get random number between 1-6 when roll dice button is clicked
+// Get random number between 1-6 when roll dice button is clicked (player one)
 function rollDicePlayerOne() {
   min = Math.ceil(1);
   max = Math.floor(7);
   const randomDiceNumb = Math.floor(Math.random() * (max - min) + min);
-  console.log(randomDiceNumb);
+  // Checks to see if number rolled was one or not
   if (randomDiceNumb === 1) {
-    console.log('its a one!');
-    rollBtnPlayer1.setAttribute("disabled", "");
-    rollBtnPlayer2.removeAttribute("disabled");
-    rollPlayer1.textContent = 'ROLL';
-    console.log('switched!');
-  } else {
     rollPlayer1.textContent = randomDiceNumb;
-  }
+    rollBtnPlayer1.setAttribute("disabled", "");
+    holdBtnPlayer1.setAttribute("disabled", "");
+    rollBtnPlayer2.removeAttribute("disabled");
+    holdBtnPlayer2.removeAttribute("disabled");
+    playerOneRoundScore = 0;
+    player1RoundScore.textContent = playerOneRoundScore + ' -';
+    
+  } else {
+    playerOneRoundScore += randomDiceNumb;
+    rollPlayer1.textContent = randomDiceNumb;
+    player1RoundScore.textContent = playerOneRoundScore + ' -'; 
+  } 
 }
 
-// Get random number between 1-6 when roll dice button is clicked
+// When the player 1 presses hold dice button
+function holdDicePlayerOne() {
+  playerOneTotalScore += playerOneRoundScore;
+  player1ScoreTotal.textContent = playerOneTotalScore;
+  playerOneRoundScore = 0;
+  player1RoundScore.textContent = playerOneRoundScore + ' -';
+  rollPlayer1.textContent = `0 `;
+  rollBtnPlayer1.setAttribute("disabled", "");
+  holdBtnPlayer1.setAttribute("disabled", "");
+  rollBtnPlayer2.removeAttribute("disabled");
+  holdBtnPlayer2.removeAttribute("disabled");
+
+}
+
+// Get random number between 1-6 when roll dice button is clicked (player two)
 function rollDicePlayerTwo() {
   min = Math.ceil(1);
   max = Math.floor(7);
   const randomDiceNumb = Math.floor(Math.random() * (max - min) + min);
   console.log(randomDiceNumb);
+  // Checks to see if number rolled was one or not
   if (randomDiceNumb === 1) {
-    console.log('its a one!');
-    rollBtnPlayer2.setAttribute("disabled", "");
-    rollBtnPlayer1.removeAttribute("disabled");
-    rollPlayer2.textContent = 'ROLL';
-    console.log('switched!');
-  } else {
     rollPlayer2.textContent = randomDiceNumb;
-  }
+    rollBtnPlayer2.setAttribute("disabled", "");
+    holdBtnPlayer2.setAttribute("disabled", "");
+    rollBtnPlayer1.removeAttribute("disabled");
+    holdBtnPlayer1.removeAttribute("disabled");
+    playerTwoRoundScore = 0;
+    player2RoundScore.textContent = playerTwoRoundScore + ' -';
+  } else {
+    playerTwoRoundScore += randomDiceNumb;
+    rollPlayer2.textContent = randomDiceNumb;
+    player2RoundScore.textContent = playerTwoRoundScore + ' -';
+  } 
 }
+
+// When the player 2 presses hold dice button
+function holdDicePlayerTwo() {
+  playerTwoTotalScore += playerTwoRoundScore;
+  player2ScoreTotal.textContent = playerTwoTotalScore;
+  playerTwoRoundScore = 0;
+  player2RoundScore.textContent = playerTwoRoundScore + ' -';
+  rollPlayer2.textContent = `0 `;
+  rollBtnPlayer2.setAttribute("disabled", "");
+  holdBtnPlayer2.setAttribute("disabled", "");
+  rollBtnPlayer1.removeAttribute("disabled");
+  holdBtnPlayer1.removeAttribute("disabled");
+}
+
